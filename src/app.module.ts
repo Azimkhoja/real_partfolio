72cba549610f6873504403d85515b2c1f93e699a
Azimkhoja <ahmadkhonovazimkhoja@.com>
2023-03-27 10:07:08 +0500

    needs of database  have been created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ProjectModule } from './project/project.module';
+import { WorkModule } from './work/work.module';
+import { UserModule } from './user/user.module';
+import { SocialModule } from './social/social.module';
+import { PostsModule } from './posts/posts.module';
+import { TagsModule } from './tags/tags.module';
+import { PostTagsModule } from './post_tags/post_tags.module';
 
 @Module({
-  imports: [],
+  imports: [ProjectModule, WorkModule, UserModule, SocialModule, PostsModule, TagsModule, PostTagsModule],
   controllers: [],
   providers: [],
 })
